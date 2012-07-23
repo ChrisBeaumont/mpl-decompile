2acfcb83b9e188e4d039315041179e0065f0822f
Chris Beaumont <beaumont@hawaii.edu>
2012-07-23 10:48:20 -0400

    First pass at a high-level interface

--- reconstruct.py
+++ reconstruct.py
@@ -5,7 +5,159 @@ import numpy as np
 
 from properties import plot_properties, scatter_properties
 
+class Reconstructor(object):
+    """ Top level class
+
+    Intended usage:
+    Reconstructor(figure_object).dump(file)
+
+    writes a self-contained pythons script to file
+
+    Note: The structure of this class follows that of pickle.Pickler
+    """
+    dispatch = {}
+
+    def __init__(self, obj):
+        self._statements = []
+        self._variables = []
+        self._taken = set()
+        self._reconstruct(obj)
+
+    def _add_statements(self, statements):
+        """Used to register new python statements to the script.
+        These will be output in the order that they were registered"""
+        if type(statements) == list:
+            self._statements.extend(statements)
+        else:
+            assert type(statements) == str
+            self._statements.append(statements)
+
+    def _import_statements(self):
+        return "import matplotlib.pyplot as plt\nimport numpy as np"
+
+    def _variable_statements(self):
+        return '\n'.join(self._variables)
+
+    def _plot_statements(self):
+        return '\n'.join(self._statements)
+
+    def _reconstruct(self, obj):
+        """ Calculate and store commands that reconstruct obj.
+
+        Returns the variable name of the reconstructed object
+        """
+        if type(obj) in self.dispatch:
+            return self.dispatch[type(obj)](self, obj)
+        else:
+            raise TypeError("Cannot reconstruct object of type %s: %s" %
+                            (type(obj), obj))
+
+    def _register_variable(self, target_name, definition):
+        """ Let's other dispatchers define variables.
+
+        These get defined together in the output script. The goal is
+        to produce cleaner syntax, so that instead of::
+
+            plt.plot(<long numpy array definition)
+
+        the output looks like::
+
+            x = <long numpy array definiton>
+            <other variable definitions>
+
+            plt.plot(x)
+
+        The registry will also disambiguate variable names to avoid
+        name conflicts
+
+        :param target name: The desired variable name (may change)
+        :param definition: python command to define the variable
+
+        :rtype: string -- the variable name that was actually used
+        (may be changed to avoid name collisions)
+
+        *Example*
+
+        >>> _register_variable('x', 'np.array([1,2,3,4,5])')
+        x_01  # name has been disambiguated
+        """
+        #XXX TODO: Disambiguate this name
+        self._variables.append("%s = %s" % (target_name, definition))
+        self._taken.add(target_name)
+        return target_name
+
+    def dump(self, outfile):
+        """ Write script to result file """
+
+        outfile.write(self._import_statements())
+        outfile.write('\n\n# variable statements\n')
+        outfile.write(self._variable_statements())
+        outfile.write('\n\n# plot statements\n')
+        outfile.write(self._plot_statements())
+
+    def dumps(self):
+        """ Return script as a string """
+        from StringIO import StringIO
+        obj = StringIO()
+        self.dump(obj)
+        return obj.getvalue()
+
+    def reconstruct_array(self, array):
+        """ Reconstruct a numpy array """
+        array = np.asarray(array)
+        dtype = array.dtype
+        string = array.tostring()
+        shape = array.shape
+
+        defn = 'np.fromstring(%r, dtype=np.%s)' % (string, dtype)
+        var = self._register_variable('arr', defn)
+        if len(shape) != 1:
+            self._add_statements('%s.shape = %r' % (var, shape))
+        return var
+
+    dispatch[np.ndarray] = reconstruct_array
+
+    def reconstruct_plot(self, artist, variable='artist'):
+        """ Reconstruct a matplotlib.lines.Line2D object into a plot call"""
+        assert isinstance(artist, Line2D)
+
+        x = artist.get_xdata()
+        y = artist.get_ydata()
+        vx = self._reconstruct(x)
+        vy = self._reconstruct(y)
+
+        result = ['%s, = plt.plot(%s, %s)' % (variable, vx, vy)]
+        result.extend(_set_properties(variable, artist, plot_properties))
+        self._add_statements(result)
+        return variable
+
+    dispatch[Line2D] = reconstruct_plot
+
+    def reconstruct_scatter(self, artist, variable='artist'):
+        """ Reconstruct a PathCollection as a call to plt.scatter """
+        assert isinstance(artist, PathCollection)
+
+        xy = artist.get_offsets()
+        xy = self._reconstruct(xy)
+
+        result = []
+        result.append('%s = plt.scatter(%s[:, 0], %s[:, 1])' %
+                      (variable, xy, xy))
+        result.extend(_set_properties(variable, artist, scatter_properties))
+        self._add_statements(result)
+        return variable
+
+    dispatch[PathCollection] = reconstruct_scatter
+    #XXX add lots more dispatch methods!
+
+
 def _set_properties(variable, reference, properties):
+    """ Create commands like variable.set_property(value), to sync to
+    properties of reference artist"""
+
+    #XXX some properties are objects like array, that need
+    #to be properly decompiled. It would be best if short definitions
+    #were defined inline, while longer ones were registered as variables
     result = []
     for prop, default in properties:
         val = getattr(reference, 'get_%s' % prop)()
@@ -18,54 +170,11 @@ def _set_properties(variable, reference, properties):
     return result
 
 
-def reconstruct_array(array, variable='x'):
-    """ Reconstruct a numpy array """
-    array = np.asarray(array)
-    dtype = array.dtype
-    string = array.tostring()
-    result = '%s = np.fromstring(%r, dtype=np.%s)' % (variable, string,
-                                                      dtype)
-    if len(array.shape) != 1:
-        result += '\n%s.shape = %r' % (variable, array.shape)
-    return result
-
-def reconstruct_plot(artist, variable='artist'):
-    """ Reconstruct a matplotlib.lines.Line2D object into a plot call"""
-
-    if not isinstance(artist, Line2D):
-        raise TypeError("Input must be a Line2D object")
-
-    x = artist.get_xdata()
-    y = artist.get_ydata()
-
-    result = []
-    result.append(reconstruct_array(x, 'x'))
-    result.append(reconstruct_array(y, 'y'))
-    result.append('%s, = plt.plot(x, y)' % variable)
-    result.extend(_set_properties(variable, artist, plot_properties))
-    return '\n'.join(result)
-
-
-def reconstruct_scatter(artist, variable='artist'):
-    """ Reconstruct a PathCollection as a call to plt.scatter """
-    if not isinstance(artist, PathCollection):
-        raise TypeError("Input must be a PathCollection")
-
-    xy = artist.get_offsets()
-
-    result = []
-    result.append(reconstruct_array(xy, 'xy'))
-    result.append('%s = plt.scatter(xy[:, 0], xy[:, 1])' % variable)
-    result.extend(_set_properties(variable, artist, scatter_properties))
-    return '\n'.join(result)
-
 def main():
+    """ Try piping this into python """
     import matplotlib.pyplot as plt
-    print 'import matplotlib.pyplot as plt'
-    print 'from numpy import *'
-    print 'import numpy as np'
-    x = plt.scatter([2,3,4,1,3,2], [2,1,2,1,1,1])
-    print reconstruct_scatter(x)
+    x, = plt.plot([1,2,3], 'r--', alpha=.3)
+    print Reconstructor(x).dumps()
     print 'plt.show()'
 
 if __name__ == "__main__":
